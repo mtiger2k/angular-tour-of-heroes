0ba2b797dd63e13a98e0ed346f57f0f92ff78e54
shaoqin.tsq <shaoqin.tsq@alibaba-inc.com>
2018-02-11 11:13:06 +0800

    add message component and service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -9,10 +10,9 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[];
-
   selectedHero: Hero;
 
+  heroes: Hero[];
 
   constructor(private heroService: HeroService) { }
 
@@ -26,6 +26,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
