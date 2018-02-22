bc510adac6c81dd613736ead6599c00fe8c557e7
shaoqin.tsq <shaoqin.tsq@alibaba-inc.com>
2018-02-22 14:23:03 +0800

    use @angular/common/http instead of @angular/http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { HttpClientModule }    from '@angular/common/http';
-import { HttpModule } from '@angular/http';
 
 //import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 //import { InMemoryDataService }  from './in-memory-data.service';
@@ -28,7 +27,7 @@ import { CanActivateAuthGuard } from './can-activate.authguard';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    HttpModule,
+
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
-import { Observable } from 'rxjs/Rx';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
  
 @Injectable()
 export class AuthenticationService {
     private authUrl = 'http://localhost:8080/auth';
-    private headers = new Headers({'Content-Type': 'application/json'});
+    private headers = new HttpHeaders({'Content-Type': 'application/json'});
  
-    constructor(private http: Http) {
+    constructor(private http: HttpClient) {
     }
  
     login(username: string, password: string): Observable<boolean> {
-        return this.http.post(this.authUrl, JSON.stringify({username: username, password: password}), {headers: this.headers})
-            .map((response: Response) => {
+        return this.http.post<any>(this.authUrl, {username: username, password: password}, {headers: this.headers})
+            .pipe(map((json: any) => {
                 // login successful if there's a jwt token in the response
-                let token = response.json() && response.json().token;
+                let token = json && json.token;
                 if (token) {
                     // store username and jwt token in local storage to keep user logged in between page refreshes
                     localStorage.setItem('currentUser', JSON.stringify({ username: username, token: token }));
@@ -28,7 +27,8 @@ export class AuthenticationService {
                     // return false to indicate failed login
                     return false;
                 }
-            }).catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+            }), catchError((error:any) => Observable.throw(error.json().error || 'Server error'))
+        );
     }
  
     getToken(): String {
